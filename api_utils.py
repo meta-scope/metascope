6dfb97978dd14e022c4897c2b412bc221c30c4b9
Shen Ting Ang <angshenting@gmail.com>
2023-04-15 22:56:59 +0900

    Added Metadata parsing function first iteration

--- api_utils.py
+++ api_utils.py
@@ -33,14 +33,14 @@ def run_metadata_query(address, nft_id, blockchain='ethereum'):
 
     return response.json()
 
-def last_transfer(metadata):
+def last_transfer(response):
     """
     Returns last transfer time
     """
     
-    if 'lastTransferTimestamp' in list(metadata.keys()):
+    if 'lastTransferTimestamp' in list(response.keys()):
         # convert the string to a datetime object
-        dt_obj = datetime.strptime(metadata['lastTransferTimestamp'], '%Y-%m-%dT%H:%M:%SZ')
+        dt_obj = datetime.strptime(response['lastTransferTimestamp'], '%Y-%m-%dT%H:%M:%SZ')
 
         # format the datetime object into a string in the desired format
         datetime_format = dt_obj.strftime('%Y-%m-%d %H:%M:%S UTC')
@@ -51,5 +51,49 @@ def last_transfer(metadata):
     
     else:
         return "Last Transfer Time undefined/missing"
+
+def process_metadata(response):
+    """
+    Does the following:
+    1. List metadata keys
+    2. Check if nested
+    3. Check if encoded JSON string or Image (skip if so!)
+    
+    
+    """
+    
+    if 'rawMetaData' in list(response.keys()):
+        # Create list of keys in metadata
+        metadata = response['rawMetaData']
+        
+        metadata_keys = list(metadata.keys())
+        
+        output_str = ''
+        # Loop through keys
+        for key in metadata_keys:
+            
+            # First check: Nested?
+            if isinstance(metadata[key], str):
+                
+                # Second check: Encoded JSON or Image
+                img_str = 'data:image'
+                json_str = 'data:json'
+                
+                if img_str in metadata[key]:
+                    output_str += f'{key} : (Image Data)\n'
+                elif json_str in metadata[key]:
+                    output_str += f'{key} : (Bas64 JSON Data)\n'
+                else:
+                    output_str += f'{key} : {metadata[key]} \n'
+            
+            else:
+                # Just print keys for now
+                output_str += str(list(metadata[key].keys()))
+            
+        return output_str
+    
+    else:
+        return "Raw Metadata undefined/missing"
+    
 
 
